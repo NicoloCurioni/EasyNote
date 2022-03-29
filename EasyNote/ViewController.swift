148066b0544703287269269ae30dacf414b2c660
Nicolò <nicolo.curioni@consulenti.hype.it>
2022-03-29 15:56:46 +0200

    Removed all Storyboard files and added a label programmatically

--- EasyNote/ViewController.swift
+++ EasyNote/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    private var mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.distribution = .fill
+        stackView.spacing = 2
+        stackView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private var labelName: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        labelName.text = "Hello, World!"
+        
+        [labelName].forEach(mainStackView.addArrangedSubview(_:))
+        
+        view.addSubview(mainStackView)
+        
+        NSLayoutConstraint.activate([
+            mainStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mainStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            labelName.centerXAnchor.constraint(equalTo: mainStackView.centerXAnchor),
+            labelName.centerYAnchor.constraint(equalTo: mainStackView.centerYAnchor),
+        ])
     }
-
-
 }
 
