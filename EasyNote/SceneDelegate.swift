cd038cd7d2fe3325853865ad57dd5951139b6e03
Nicolò <nicolo.curioni@consulenti.hype.it>
2022-03-29 17:55:38 +0200

    Code refactoring in `NoteListController`

--- EasyNote/SceneDelegate.swift
+++ EasyNote/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: HomeController())
+        window?.rootViewController = UINavigationController(rootViewController: AddNoteController())
         window?.makeKeyAndVisible()
     }
 
--- EasyNote/Scenes/Add/AddNoteController.swift
+++ EasyNote/Scenes/Add/AddNoteController.swift
@@ -8,11 +8,21 @@
 import UIKit
 
 class AddNoteController: UITableViewController {
+    
+    private var cancelBarButtonItem: UIBarButtonItem = {
+        let barButton = UIBarButtonItem()
+        return barButton
+    }()
+    
+    private var saveBarButtonItem: UIBarButtonItem = {
+        let barButton = UIBarButtonItem()
+        return barButton
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        title = "New Note"
+
+        setupUI()
     }
 
     // MARK: - Table view data source
@@ -28,4 +38,34 @@ class AddNoteController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    // MARK: - Actions
+    
+    // Private
+    
+    @objc
+    private func didTapCancel() {
+        print(#function)
+    }
+    
+    @objc
+    private func didTapSave() {
+        print(#function)
+    }
+    
+    // Setup UI
+    private func setupUI() {
+        
+        // Title
+        title = "New Note"
+        
+        // Bar Buttons
+        cancelBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(didTapCancel))
+        saveBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(didTapSave))
+        saveBarButtonItem.style = .done
+        
+        navigationItem.leftBarButtonItem = cancelBarButtonItem
+        navigationItem.rightBarButtonItem = saveBarButtonItem
+    }
+    
 }
